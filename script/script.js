ca4bf6bfbf4348bbc019e318ab4ba2974aa202e5
Eduardo Schmitt <56314623+eduardoschmitt@users.noreply.github.com>
2022-08-15 14:41:47 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const vetor = [];
+const botao = document.getElementById("btn");
+const color = document.querySelector(".color");
+var stringColor = '#';
+
+botao.addEventListener("click", () => {
+
+    let stringColor = '#';
+    while (stringColor.length < 7) {
+        stringColor += Math.floor(Math.random() * 0x10).toString(16);
+    }
+    console.log(stringColor);
+
+    document.body.style.backgroundColor = stringColor;
+    color.textContent = stringColor;
+    stringColor = '#'
+})
+
+function getRandomNumber(){
+   return Math.floor(Math.random() * 16);
+}
